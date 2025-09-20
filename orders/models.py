274c84334aaf13cfb1a50d13b6527332f17f31f5
Nandhinidevi P S <169060763+Nandhinidevi01@users.noreply.github.com>
2025-09-20 18:15:05 +0530

    49837

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+from .models import OrderStatus # if OrderStatus is in the same app(orders)
+#or: from orders.models import OrderStatus
+
+class order(models.Model):
+    # existing fields
+    customer = models.CharField(max_length=200)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    Created_at = models.DateTimeField(auto_now_add=True)
+
+    #Now field for order status
+    status = models.ForeignKey(
+        OrderStatus,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="orders"
+    )
+
+    def __str__(self):
+        return f"Order #{self.id} - {self.customer}"
