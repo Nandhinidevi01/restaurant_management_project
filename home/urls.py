51dcbca178a49cccee0cf73ef563c4503c92d188
Nandhinidevi P S <169060763+Nandhinidevi01@users.noreply.github.com>
2025-09-21 19:08:51 +0530

    49840

--- home/urls.py
+++ home/urls.py
@@ -4,3 +4,9 @@ from .views import *
 urlpatterns = [
     
 ]
+
+from home.views import MenuCategoryListView
+
+urlpatterns = [
+    path('categories/', MenuCategoryListView.as_view(), name='menu-categories'),
+]
