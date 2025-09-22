c621497b05b167028e4066f6627093b18b78569b
Nandhinidevi P S <169060763+Nandhinidevi01@users.noreply.github.com>
2025-09-22 19:08:42 +0530

    50151

--- home/serializers.py
+++ home/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from home.models import MenuCategory
+from home.models import MenuItem  #assuming you have a MenuItem model
 
 class MenuCategorySerializer(serializers.ModelSerializer):
     """
@@ -9,4 +10,12 @@ class MenuCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuCategory
         fields = ['id', 'name'] #include id for frontend reference
+        
+class MenuItemSerializer(serializers.ModelSerializer):
+    """
+    Serializer for MenuItem model.
+    """
+    class Meta:
+        model = MenuItem
+        fields = ['id', 'name', 'price', 'description', 'category'] # adjust fields as per your model
         
