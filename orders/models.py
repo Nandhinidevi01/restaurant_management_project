57f8d334dc4ff1774a04e012fdcff76cafcd22e5
Nandhinidevi P S <169060763+Nandhinidevi01@users.noreply.github.com>
2025-09-23 19:09:05 +0530

    50155

--- orders/models.py
+++ orders/models.py
@@ -3,6 +3,10 @@ from django.db import models
 # Create your models here.
 from .models import OrderStatus # if OrderStatus is in the same app(orders)
 #or: from orders.models import OrderStatus
+from django.contrib.auth.models import User
+from menu.models import MenuItem
+from .models import OrderStatus
+
 
 class order(models.Model):
     # existing fields
@@ -44,3 +48,21 @@ class Coupon(models.Model):
 
     def __str__(self):
         return f"{self.code} - {self.discount}%"
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
+    Created_at = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.ForeignKey(OrderStatus, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"Order {self.id} by {self.user.username}"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.menu_item.name} (x{self.quantity})"
+        
