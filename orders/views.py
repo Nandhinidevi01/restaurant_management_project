aa6ad0074a7c49839c51024801dbf463ec03d87c
Nandhinidevi P S <169060763+Nandhinidevi01@users.noreply.github.com>
2025-09-23 19:24:40 +0530

    50155

--- orders/views.py
+++ orders/views.py
@@ -4,6 +4,13 @@ from django.shortcuts import render
 from rest_framework.response import Response 
 from rest_framework import status, views
 from utils.validation_utils import validate_email_address
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from .models import Order
+from .serializers import OrderSerializer
+
 
 class SignupView(views.APIView):
     def post(self, request):
@@ -20,3 +27,18 @@ class SignupView(views.APIView):
             {"message": "Email validated_successfully"},
             status=status.HTTP_200_OK
         )
+
+
+class OrderHistoryView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        """
+        Retrieve order history for the logged in user.
+        """
+
+        user = request.user
+        orders = Order.objects.filter(user=user).order_by('-created_at')
+        serializer = OrderSerializer(orders, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+        
