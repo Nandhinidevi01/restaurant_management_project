b59c11cf38bc9b73ac40e997221e4caa2fe8b67a
Nandhinidevi P S <169060763+Nandhinidevi01@users.noreply.github.com>
2025-09-25 15:29:46 +0530

    50795

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from .views import *
+from .views import UserProfileViewSet
+
 
 urlpatterns = [
     path('items/', ItemView.as_view(), name='item-list'),
+]
+
+urlpatterns1 = [
+    path('profile/update/', user_profile_update, name="user-profile-update",)
 ]
