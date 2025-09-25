11232f0cd5ba9c619274d4a4788ead182bcb1498
Nandhinidevi P S <169060763+Nandhinidevi01@users.noreply.github.com>
2025-09-25 15:20:55 +0530

    50795

--- products/serializers.py
+++ products/serializers.py
@@ -5,3 +5,13 @@ class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = '__all__'
+
+from django.contrib.auth.models import User
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["first_name", "last_name", "email"]
+        extra_kwargs = {
+            "email": {"required": True}
+        }
