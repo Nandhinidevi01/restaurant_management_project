e8e25361bed1e9aaa822247c49d3d78b3cfd7cd1
Nandhinidevi P S <169060763+Nandhinidevi01@users.noreply.github.com>
2025-09-29 19:47:05 +0530

    52155

--- home/models.py
+++ home/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
 class MenuCategory(models.Model):
-    """
-    Represents a category for the resturant's menu
-    """
-    name = models.CharField(max_length = 100, unique = True)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
+
+class ContactFormSubmission(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.email}"
