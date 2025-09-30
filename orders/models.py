fedea9dc9f74a4f8271f7ad83e9232ccaac9f3a1
Nandhinidevi P S <169060763+Nandhinidevi01@users.noreply.github.com>
2025-09-30 14:12:35 +0530

    52158

--- orders/models.py
+++ orders/models.py
@@ -6,6 +6,7 @@ from .models import OrderStatus # if OrderStatus is in the same app(orders)
 from django.contrib.auth.models import User
 from menu.models import MenuItem
 from .models import OrderStatus
+from decimal import Decimal 
 
 
 class order(models.Model):
@@ -117,3 +118,33 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order {self.id} - {self.status}"
+
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('Preocessing', 'Processing'),
+        ('Cancelled', 'Cancelled'),
+        ('Completed', 'Completed'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="Orders")
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    Created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pending')
+
+    def __str__(self):
+        return f"Order {self.id} - {self.status}"
+
+        def calculate_total(self):
+            total = Decimal("0.00")
+            for item in self.items.all():
+                total += item.price * item.quantity
+            return total
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    menu_item = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.menu_item.name} (order {self.order.id})"
