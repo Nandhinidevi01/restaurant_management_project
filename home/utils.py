86b92914f436f2f88bc72c6419f289877c9710b2
Nandhinidevi P S <169060763+Nandhinidevi01@users.noreply.github.com>
2025-10-01 12:37:43 +0530

    52647

--- home/utils.py
+++ home/utils.py
@@ -1,6 +1,8 @@
 from django.core.mail import send_mail, BadHeaderError
 from django.conf import settings
 import logging
+from datetime import datetime, time
+from home.utils import is_restaurant_open
 
 logger = logging.getLogger(__name__)
 
@@ -31,3 +33,36 @@ def send_email(recipient_email, subject, message_body, from_email=None):
     except Exception as e:
         logger.error(f"Error sending email: {e}")
         return False
+
+def is_restaurant_open():
+    """
+    Check if the restaurant is currently open based on operating hours.
+
+    Returns:
+        bool: True if open, False if closed.
+    """
+
+    #get current day and time
+    now = datetime.now()
+    current_time = now.time()
+    current_day = now.strftime("%A")
+
+    opening_hours = {
+        "Monday":    (time(9,0), time(22,0)),
+        "Tuesday":   (time(9,0), time(22,0)),
+        "Wednesday": (time(9,0), time(22,0)),
+        "Thursday":  (time(9,0), time(22,0)),
+        "Friday":    (time(9,0), time(23,0)),
+        "Saturday":  (time(9,0), time(23,0)),
+        "Sunday":    (time(10,0), time(21,0)),
+    }
+
+    #Get today's hours
+    if current_day not in opening_hours:
+        return False
+
+    open_time, close_time = opening_hours[current_day]
+
+    return open_time <= current_time <= close_time
+
+print(is_restaurant_open())
