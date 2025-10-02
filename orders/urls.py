227865ac1f37f8a8ed73d51037b704c83c1569b3
Nandhinidevi P S <169060763+Nandhinidevi01@users.noreply.github.com>
2025-10-02 11:49:01 +0530

    52650

--- orders/urls.py
+++ orders/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import OrderHistoryView
 from .views import OrderDetailView
 from .views import CancelOrderView
+from .views import CouponValidationView
 
 
 urlpatterns = [
@@ -14,4 +15,8 @@ urlpatterns1 = [
 
 urlpatterns2 = [
     path('orders/<int:order_id>/cancel/', CancelOrderView.as_view(), name='Cancel-order'),
+]
+
+urlpatterns3 = [
+    path('coupons/validate/', CouponValidationView.as_view(), name='coupon_validate')
 ]
