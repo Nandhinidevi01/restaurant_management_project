795de71e14e50eba2baf883ecfcc68b4c61fbe86
Nandhinidevi P S <169060763+Nandhinidevi01@users.noreply.github.com>
2025-10-02 11:47:22 +0530

    52650

--- orders/views.py
+++ orders/views.py
@@ -14,6 +14,9 @@ from rest_framework.generics import RetrieveAPIView
 from .serializers import OrderDetailSerializer
 from orders.utils import send_order_confirmation_email
 from django.shortcuts import get_object_or_404
+from django.utils import timezone
+from .models import Coupon
+from .serializers import CouponSerializer
 
 
 class SignupView(views.APIView):
@@ -84,3 +87,27 @@ class CancelOrderView(APIView):
         return Response({"message": "Order Cancelled successfully.",
                         "order": OrderSerializer(order).data},
                         status=status.HTTP_200_OK)
+
+class CouponValidationView(APIView):
+    def post(self, request, *args, **kwargs):
+        code = request.data.get('code')
+
+        if not code:
+            return Response({"error": "Coupon code is required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            Coupon = Coupon.objects.get(code=code, is_active=True)
+        except Coupon.DoesNotExist:
+            return Response({"error": "Invalid or inactive coupon code"}, status=status.HTTP_400_BAD_REQUEST)
+
+        today = timezone.now().date()
+        if coupon.valid_from <= today <= coupon.valid_until:
+            serializer = CouponSerializer(coupon)
+            return Response({
+                "success": True,
+                "discount_percentage": coupon.discount_percentage,
+                "coupon": serializer.data
+            }, status=status.HTTP_200_OK)
+
+        else:
+            return Response({"error": "Coupon is expired or not yet valid"}, status=status.HTTP_400_BAD_REQUEST)
