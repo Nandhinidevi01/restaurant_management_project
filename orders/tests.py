d40b642888c5996c57b3a5c4af33d248abd839ec
Nandhinidevi P S <169060763+Nandhinidevi01@users.noreply.github.com>
2025-10-07 21:33:31 +0530

    53672

--- orders/tests.py
+++ orders/tests.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from home.models import MenuItem
-from .models import Order, OrderItem
+from .models import Order, OrderItem, Customer
+from decimal import Decimal 
+from django.contrib.auth import get_user_model
+
 
 class OrderModelTest(TestCase):
     def setup(self):
@@ -25,3 +28,26 @@ class OrderModelTest(TestCase):
         def save(self, *args, **kwargs):
             self.total_price = self.calculate_total()
             super().save(*args, **kwargs)
+
+class OrderCalculatedTotalTest(TestCase):
+    def setup(self):
+        User = get_user_model()
+        self.user = User.objects.create_user(username='u1', password='p')
+        self.order = Order.objects.create(user=self.user)
+
+        mi = MenuItem.objects.create(
+            restaurant_id=1, name="Test_item", price=Decimal('120.00'), is_available=True
+        )
+        OrderItem.objects.create(order=self.order, menu_item=mi, quantity=2, price=Decimal('120.00'))
+        mi2 = MenuItem.objects.create(
+            restaurant_id=1, name="Side", price=Decimal('30.00'), is_available=True
+        )
+        OrderItem.objects.create(order=self.order, menu_item=mi2, quantity=3, price=Decimal('30.00'))
+
+    def test_calculate_total_without_discount(self):
+        total = self.order.calculate_total()
+        self.assertEqual(total, Decimal('330.00'))
+    def test_calculate_total_and_save(Self):
+        total = self.order.calculate_total(save=True)
+        self.order.refresh_from_db()
+        self.assertEqual(self.order.total_price, Decimal('330.00'))
