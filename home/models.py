5b9a9901015a811e4454c86e2f14cc30ed8f5c68
Nandhinidevi P S <169060763+Nandhinidevi01@users.noreply.github.com>
2025-10-11 18:24:16 +0530

    53673

--- home/models.py
+++ home/models.py
@@ -14,3 +14,13 @@ class ContactFormSubmission(models.Model):
 
     def __str__(self):
         return f"{self.name} - {self.email}"
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    is_daily_special = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+        
