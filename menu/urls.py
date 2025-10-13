0254280f097abc5ec19ee899ce685980ff1499f1
Nandhinidevi P S <169060763+Nandhinidevi01@users.noreply.github.com>
2025-10-13 21:16:05 +0530

    54717

--- menu/urls.py
+++ menu/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from .views import MenuItemsByCategoryView
+from django.contrib import admin
 
 urlpatterns = [
     path('menu-items-by-category/', MenuItemsByCategoryView.as_view(), name='menu-items-by-category'),
+]
+
+urlpatterns1 = [
+    path('admin/', admin.site.urls),
+    path('api/home/', include('home.urls')),
 ]
