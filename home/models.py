e805f1f97792cf06c94c7ba506aafeba39ac69be
Nandhinidevi P S <169060763+Nandhinidevi01@users.noreply.github.com>
2025-10-13 20:25:18 +0530

    54715

--- home/models.py
+++ home/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class MenuCategory(models.Model):
     name = models.CharField(max_length=100, unique=True)
@@ -23,4 +25,21 @@ class MenuItem(models.Model):
 
     def __str__(self):
         return self.name
-        
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+class UserReview(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE, related_name='reviews')
+    rating = models.PositiveIntegerField()
+    comment = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.menu_item.name} ({self.rating})"
