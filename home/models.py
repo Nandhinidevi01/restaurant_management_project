8173ea428ee100cd843aece6abd981f0b4ab4e7b
Nandhinidevi P S <169060763+Nandhinidevi01@users.noreply.github.com>
2025-10-29 19:45:39 +0530

    56668

--- home/models.py
+++ home/models.py
@@ -103,3 +103,24 @@ class MenuItem(models.Model):
 
     def __str__(self):
         return self.name
+        
+class OpeningHour(models.Model):
+    DAY_CHOICES = [
+        ('Monday', 'Monday'),
+        ('Tuesday', 'Tuesday'),
+        ('Wednesday', 'Wednesday'),
+        ('Thursday', 'Thursday'),
+        ('Friday'), ('Friday'),
+        ('Saturday'), ('Saturday'),
+        ('Sunday'), ('Sunday'),
+    ]
+
+    day = models.CharField(max_length=10, choices=DAY_CHOICES, unique=True)
+    opening_time = models.TimeField()
+    closing_time = models.TimeField()
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return f"{self.day}: {self.opening_time} - {self.closing_time}"
